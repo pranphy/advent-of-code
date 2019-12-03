8cace6c5cacad51108c4900dfa74806041a12be5
Prakash Gautam <pranphy@gmail.com>
2019-12-03 13:11:39 -0500

    Solve problem one, BRAVO

---
+++ problems/1Tyranny.jl
@@ -0,0 +1,35 @@
+# author : Prakash
+# date   : Tue Dec  3 12:27:05 EST 2019
+
+masses = readlines("1module_masses.txt")
+
+function fuel_for_mass(mass)
+    floor(mass/3)-2
+end
+
+
+function rec_fuel(mass)
+    if fuel_for_mass(mass) <= 0
+        return 0
+    else
+        fuel = fuel_for_mass(mass)
+        return fuel + rec_fuel(fuel)
+    end
+end
+
+sum = 0
+rec_sum = 0
+
+for mass in masses
+    mass = tryparse(Float64,mass)
+    global sum += fuel_for_mass(mass)
+end
+println(sum)
+
+for mass in masses
+    mass = tryparse(Float64,mass)
+    global rec_sum += rec_fuel(mass)
+end
+
+println(rec_sum)
+
