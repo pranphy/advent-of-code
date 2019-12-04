2c4029645a39666b12b598f98ada8fb55f63b441
Prakash Gautam <pranphy@gmail.com>
2019-12-04 14:25:44 -0500

    Complete problem 4

---
+++ problems/4PasswordGuess.jl
@@ -0,0 +1,64 @@
+#!/usr/bin/env julia
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 ft=julia
+
+# author : Prakash [प्रकाश]
+# date   : 2019-12-04 13:44
+
+function is_monotonic(number)
+    numstr = string(number)
+    old = '0'
+    for x in numstr
+        if x < old
+            return false
+        else
+            old = x
+        end
+    end
+    return true
+end
+    
+
+function has_double(number)
+    numstr = string(number)
+    for (i,c)  in enumerate(numstr)
+        if c in numstr[i+1:length(numstr)]
+            return true
+        end
+    end
+    return false
+end
+
+function count_digits(number)
+    numstr = string(number)
+    counts = Vector{Int64}()
+    for x in numstr
+        cnt = count(i -> (i==x), numstr)
+        push!(counts,cnt)
+    end
+    return counts
+end
+
+criteria1(list) = maximum(list) >= 2
+criteria2(list)  =  2 in list
+
+function get_count(criteria)
+    first = 156666
+    last  = 652527
+    count = 0
+
+    for x = first:last
+        if is_monotonic(x)
+            counts = count_digits(x)
+            count += criteria(counts)
+        end
+    end
+    return count
+end
+
+count1 = get_count(criteria1)
+count2 = get_count(criteria2)
+
+println("There are a total of ",count1, " matching first criteria")
+println("There are a total of ",count2, " matching second criteria")
+
