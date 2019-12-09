759ebb28f219fda4747900b10d5b58f691d6b63b
Prakash Gautam <pranphy@gmail.com>
2019-12-08 23:25:58 -0500

    Complete problem 5 part 2

--- problems/5Diagnostic.jl
+++ problems/5Diagnostic.jl
@@ -14,31 +14,36 @@ function read_program_array()
 end
 
 op_length(x) = Dict(1 => 3, 2 => 3, 3 => 1, 4 => 1, 5 => 2, 6 =>2, 7=>3, 8 => 3)[x]
-op_func(x) = Dict(1 => (x,y) -> x+y, 2 => (x,y) -> x*y, 3 =>  x -> x , 4 => x -> x)[x]
+op_func(x) = Dict(1 => (x,y) -> x+y, 2 => (x,y) -> x*y, 7 => (x,y) -> x < y ? 1 : 0, 8 => (x,y) -> x == y ? 1 : 0)[x]
 
 function decompose(command)
     opcode = command % 100
     mode = command ÷ 100 # Int64(floor(command / 100)) # how about this
 
-    modes = [digits(mode); zeros(op_length(opcode)-length(digits(mode)))]
+    modes =map(x -> Int64(x), [digits(mode); zeros(op_length(opcode)-length(digits(mode)))])
     return opcode,modes
 end
 
 function process_blocks(program_register,pos,input=1)
     opcode,modes = decompose(program_register[pos])
     param_count = op_length(opcode)
-    if opcode == 3
-        address = program_register[pos+1]
-        program_register[address+1] = input
+    jump = param_count + 1
+    block = program_register[pos+1:pos+param_count]
+    params = map(i -> modes[i] == 0 ? program_register[block[i]+1] : block[i],1:param_count)
+    if opcode in [1,2,7,8]
+        program_register[block[end]+1] = op_func(opcode)(params[1],params[2])
+    elseif opcode == 3
+        program_register[block[end]+1] = input
     elseif opcode == 4
-        address = program_register[pos+1]
-        println(" julia@mars: ~ : ",program_register[address+1])
-    elseif opcode in [1,2]
-        block = program_register[pos+1:pos+param_count]
-        params = map(i -> modes[i] == 0 ? program_register[block[i]+1] : block[i],1:param_count-1)
-        program_register[block[3]+1] = op_func(opcode)(params[1],params[2])
+        println(" julia@mars~: ",program_register[block[end]+1])
+    elseif opcode == 5
+        pos = params[1] != 0 ? params[end] + 1 : pos + jump
+        jump = 0
+    elseif opcode == 6
+        pos = params[1] == 0 ? params[end] + 1 : pos + jump
+        jump = 0
     end
-    pos += param_count + 1
+    pos += jump
     return program_register,pos
 end
 
@@ -53,10 +58,12 @@ function run_program(program_register,inp)
 end
 
 
-function doit()
-    program_register = read_program_array()
-    println("Part one :: ")
-    run_program(program_register,1)
-end
 
-doit()
+
+program_register = read_program_array()
+println("Part one :: ")
+run_program(program_register,1)
+program_register = read_program_array()
+println("Part Two:: ")
+run_program(program_register,5)
+
