4447764027465bdde0ba127ee87437cf7a7ae453
Prakash Gautam <pranphy@gmail.com>
2019-12-17 10:27:03 -0800

    Start Restructure for the seven part two

--- problems/7MaxThrust.jl
+++ problems/7MaxThrust.jl
@@ -6,18 +6,61 @@
 # date   : 2019-12-09 11:02
 
 using DataStructures
-import Combinatorics; const cmb=Combinatorics
+import Combinatorics
+
+
+read_program_array(filename)  =  map(x -> tryparse(Int64,x), split(readlines(filename)[1],","))
+PROGRAM_ARRAY = read_program_array("./files/7maximum_thrust.txt")
 
 input_buffer = Queue{Int}()
 output_buffer = Queue{Int}()
 
-function read_program_array()
-    opcodes = readlines("./files/7maximum_thrust.txt")[1]
-    map(x -> tryparse(Int64,x), split(opcodes,","))
+
+struct State
+    type::String
+    opcode::Int64
 end
 
+struct Instruction
+    pos::Int64
+    command::Int64
+    opcode::Int64
+    modes::Vector{Int64}()
+    params::Vector{Int64}()
+    out::Int64
+end
+
+struct Amplifier
+    program_reg::Array{Int64}()
+    current_block::Instruction
+    state::State
+end
+
+
+function initialize_array(num_blocks=5)
+    amp_chain = Vector{Amplifier}(num_blocks)
+    for i in 1:num_blocks:
+        program_reg = copy(PROGRAM_ARRAY)
+        instrution = Instruction(1,1,1,[],[],1)
+        state = State("Beg")
+        amplifier = Amplifier(program_reg,intruction,state)
+        amp_chain[i] = amplifier
+    end
+end
+
+
+ADD(ins::Instruction) = ins.out = ins.params[1]+ins.params[2]
+MUL(ins::Instruction) = ins.out = ins.params[1]*ins.params[2]
+INP(ins::Instruction) = if isempty(input_buffer)  State("WAIT") else ins.pos += 2; dequeue!(input_buffer) end
+OUT(ins::Instruction) = enqueue!(output_buffer,inp.params[end])
+JIT(ins::Instruction) = ins.pos = ins.param[0] != 0 ?  ins.params[end] + 1 : ins.pos + 2
+JIF(ins::Instruction) = ins.pos = ins.param[0] == 0 ?  ins.params[end] + 1 : ins.pos + 2
+LTI(ins::Instruction) = ins.out = ins.param[0] < ins.parms[1] ? 0 : 1
+EQI(ins::Instruction) = ins.out = ins.param[0] == ins.parms[1] ? 0 : 1
+
+
 op_length(x) = Dict(1 => 3, 2 => 3, 3 => 1, 4 => 1, 5 => 2, 6 =>2, 7=>3, 8 => 3)[x]
-op_func(x) = Dict(1 => (x,y) -> x+y, 2 => (x,y) -> x*y, 7 => (x,y) -> x < y ? 1 : 0, 8 => (x,y) -> x == y ? 1 : 0)[x]
+op_func(x) = Dict(1 => ADD, 2 => MUL, 3 => INP, 4=> OUT, 5 => JIT, 6 => JIF, 7=> JIL, 8 => JIE)[x]
 
 function decompose(command)
     opcode = command % 100
@@ -33,11 +76,8 @@ function process_blocks(program_register,pos)
     jump = param_count + 1
     block = program_register[pos+1:pos+param_count]
     params = map(i -> modes[i] == 0 ? program_register[block[i]+1] : block[i],1:param_count)
-    if opcode in [1,2,7,8]
-        program_register[block[end]+1] = op_func(opcode)(params[1],params[2])
-    elseif opcode == 3
-        input = dequeue!(input_buffer)
-        program_register[block[end]+1] = input
+    if opcode in [1,2,3,7,8]
+        program_register[block[end]+1] = op_func(opcode)(params)
     elseif opcode == 4
         enqueue!(output_buffer,program_register[block[end]+1])
     elseif opcode == 5
@@ -56,28 +96,37 @@ function run_program(program_register)
     while program_register[pos] != 99
         program_register,pos = process_blocks(program_register,pos)
     end
+    return pos == 1 ? 1 : 0
 end
 
 function find_thrust(combination)
+    amplifier_register = Dict(x => read_program_array() for x in 1:length(combination))
     prev_output = 0 # first input is zero
-    for phase in combination
-        program_register = read_program_array()
+    while not halt
+        for (i,phase) in enumerate(combination)
+            program_register = amplifier_register[i]
 
-        empty!(input_buffer)
-        empty!(output_buffer)
+            empty!(input_buffer)
+            empty!(output_buffer)
 
-        enqueue!(input_buffer,phase)
-        enqueue!(input_buffer,prev_output)
+            enqueue!(input_buffer,phase)
+            enqueue!(input_buffer,prev_output)
 
-        run_program(program_register)
+            run_program(program_register)
 
-        prev_output = back(output_buffer)
+            prev_output = back(output_buffer)
+        end
     end
     return prev_output
 end
 
 
-phase_combinations = cmb.permutations([0,1,2,3,4])
+phase_combinations = Combinatorics.permutations([0,1,2,3,4])
 thrusts = map(find_thrust,phase_combinations)
 println(" Maximum thrust is $(maximum(thrusts)) ")
 
+function run_block(num):
+    if input:
+        if output_buffer is empty:
+            op = 
+        deque(output_buffer)
