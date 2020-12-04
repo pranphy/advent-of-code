eeb8288531341af6fbf3201652808204c7c6162b
Prakash Gautam <pranphy@gmail.com>
2020-12-04 14:33:51 -0500

    Refactor problem 2

--- 2020/2_password-philosophy.jl
+++ 2020/2_password-philosophy.jl
@@ -8,41 +8,30 @@ function read_file(filename)
     [(split(x,"-")[1],split(split(x,"-")[2]," ")[1], split(split(x," ")[2],":")[1], split(x,": ")[2]) for x in policies]
 end
 
-function check_valid_policy1(info)
-    valid = 0
-    for policy in info
-        min= tryparse(Int64,policy[1]) 
-        max = tryparse(Int64,policy[2])
-        char = policy[3]
-        password = policy[4]
-        chars = [string(char) == string(cur) ? 1 : 0 for cur in password]
-        total = sum(chars)
-        if total >= min && total <= max
-            valid += 1
-        end
-    end
-    valid
+function philosophy1(min,max,char,password)
+    total = sum([string(char) == string(cur) ? 1 : 0 for cur in password])
+    total >= min && total <= max
 end
 
-function check_valid_policy2(info)
+function philosophy2(min,max,char,password)
+    (string(password[min]) == string(char)) ⊻ (string(char) == string(password[max]))
+end
+
+function check_valid(info,philosophy)
     valid = 0
     for policy in info
-        min = tryparse(Int64,policy[1])
+        min = tryparse(Int64,policy[1]) 
         max = tryparse(Int64,policy[2])
         char = policy[3]
         password = policy[4]
-        cond1 = string(password[min])==string(char) ? 1 : 0
-        cond2 = string(password[max])==string(char) ? 1 : 0
-        if cond1 + cond2 == 1
-            valid +=1
-        end
+        valid += philosophy(min,max,char,password) ? 1 : 0
     end
     valid
 end
 
 info = read_file("./files/2_password-philosophy.txt")
 
-total_valid_1 = check_valid_policy1(info)
-total_valid_2 = check_valid_policy2(info)
+total_valid_1 = check_valid(info,philosophy1)
+total_valid_2 = check_valid(info,philosophy2)
 println("There are $(total_valid_1) valid passwords in the file with policy 1")
 println("There are $(total_valid_2) valid passwords in the file with policy 2")
